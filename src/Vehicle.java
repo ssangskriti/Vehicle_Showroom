ef487f4e9df4b4f74b6d889eb9faac78090738fa
Sristy Sangskriti <sristysangskriti007@gmail.com>
2020-07-05 22:31:26 +0600

    finalized

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,24 +1,47 @@
+import java.util.ArrayList;
+
+
 abstract class Vehicle {
 
-    protected int model_number;
-    protected int engine_power;
-    protected int tire_size;
-    protected String engine_type;
+     String type;
+     int model_number;
+     int engine_power;
+     int tire_size;
+     String engine_type;
 
-    Vehicle(int model_number, String engine_type, int engine_power, int tire_size ){
+    static ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+    static int visitorCounter = 30;
 
+    Vehicle(String type, int model_number, String engine_type, int engine_power, int tire_size ){
+
+        this.type = type;
         this.model_number = model_number;
         this.engine_type = engine_type;
         this.engine_power = engine_power;
         this.tire_size = tire_size;
+
+        if(engine_type=="Sports")
+            visitorCounter += 20;
     }
 
-    abstract void addVehicle(int model_number, String engine_type, int engine_power, int tire_size);
+    abstract void addVehicle(Vehicle v);
 
-    abstract void removeVehicle(int model_number);
+    static void removeVehicle(int model_number){
+        for(Vehicle v: vehicleList)
+            if(v.model_number==model_number){
+                vehicleList.remove(v);
+                break;
+            }
+    }
 
-    abstract void showVehicleList();
+    static void showVehicleList(){
+        System.out.println("Current list of vehicle in the showroom: ");
+        for(Vehicle v: vehicleList)
+            System.out.println(v.model_number+" "+v.engine_type+" "+v.engine_power+" "+v.type+"\n");
+    }
 
-    abstract void showVisitorCount();
+    static void showVisitorCount(){
+        System.out.println("Current expected visitors: "+ visitorCounter);
+    }
 
 }
